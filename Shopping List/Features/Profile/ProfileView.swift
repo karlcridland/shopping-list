5ae803f3280351d63793eeb99efbf0a06b27835a
Karl Cridland <karlcridland@gmail.com>
2025-09-15 16:07:13 +0100

    Shopper information loaded correctly into profile page, preview added.

--- Shopping List/Features/Profile/ProfileView.swift
+++ Shopping List/Features/Profile/ProfileView.swift
@@ -6,14 +6,29 @@
 //
 
 import SwiftUI
+import CoreData
 
 struct ProfileView: View {
     
-//    @ObservedObject var viewModel: ProfileViewModel
-    @State var shopper: Shopper
+    @ObservedObject var viewModel: ProfileViewModel
     
-    var body: some View {
-        Text(shopper.name.full)
+    init(shopper: Shopper) {
+        self.viewModel = ProfileViewModel(shopper: shopper)
     }
     
+    var body: some View {
+        Text(viewModel.shopper.name.full)
+    }
+    
+}
+
+
+#Preview {
+    let context = PersistenceController.preview.container.viewContext
+    let shopper = Shopper(context: context)
+    shopper.givenName = "Karl"
+    shopper.familyName = "Cridland"
+    
+    return ProfileView(shopper: shopper)
+        .environment(\.managedObjectContext, context)
 }
--- Shopping List/Features/Profile/ProfileViewModel.swift
+++ Shopping List/Features/Profile/ProfileViewModel.swift
@@ -5,6 +5,18 @@
 //  Created by Karl Cridland on 09/09/2025.
 //
 
+import Foundation
+
 class ProfileViewModel: ObservableObject {
+    
+    let shopper: Shopper
+    
+    init(shopper: Shopper) {
+        self.shopper = shopper
+    }
+    
+    func blockShopper() {
+        
+    }
     
 }
